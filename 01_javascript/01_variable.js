52d9610a827abcc3f90422b282d76574ceb745f5
Ayush Kumar Singh <147124011+AyushKumarSingh866@users.noreply.github.com>
2024-01-27 16:00:27 +0000

    datatypes

--- 01_javascript/01_variable.js
+++ 01_javascript/01_variable.js
@@ -2,14 +2,15 @@ const accountId=144559
 let accountEmail="ayushjack44@gmail.com"
 var accountPassword="12345"
 accountCity="Ranchi"
-
+let accountState;
 // accountId=2 // not allowed
 accountEmail= "hc@AbortController.com"
 accountPassword= "455654"
 accountCity="Bhubaneshwar"
-let accountState;
+;
 /*
 prefer not to use var because of issue in block scope and functional scope*/ 
+accountEmail="abcm@google.com"
 console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
