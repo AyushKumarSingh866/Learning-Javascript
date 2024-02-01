55f714c4d66072aa339f0f5ed963712d66fa7806
Ayush Kumar Singh <147124011+AyushKumarSingh866@users.noreply.github.com>
2024-02-01 19:15:30 +0000

    Datatype summary

---
+++ 01_javascript/Datatypes_summary.js
@@ -0,0 +1,28 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 65448895477405556554n 
+
+// Array,object,function
+ const heros = ["Ironman", "Thor" , "Spiderman"];
+
+ let myObj = {
+
+    name: "Ayush",
+    age: 22,
+
+ }
+
+ const myFunction = function(){
+   console.log("Hello World");
+}
+console.log(typeof myFunction);
