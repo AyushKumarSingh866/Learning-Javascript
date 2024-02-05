66da8225ac52862e39ec2a183817e0db985e76b8
Ayush Kumar Singh <147124011+AyushKumarSingh866@users.noreply.github.com>
2024-02-05 07:41:59 +0000

    JSON api intro and destructure of Objects

---
+++ 02_javascript/05_destructureOfObjects.js
@@ -0,0 +1,26 @@
+const course = {
+
+    coursename: '1100xcohort',
+    price: "6000",
+    courseInstructor: "Harkirat"
+    
+  
+  }
+
+//   course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+
+// {
+//   "name": "Ayush",
+//   "coursename": "Devxtools",
+//   "price":"1000"
+
+// }
+
+
+
+
