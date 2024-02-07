577f97fc1c49ee8bc10e54cb10dfe4e476e8d3f5
Ayush Kumar Singh <147124011+AyushKumarSingh866@users.noreply.github.com>
2024-02-07 17:41:42 +0000

    scopes is javascript

---
+++ 03_javascript/02_scopes.js
@@ -0,0 +1,64 @@
+// var c = 300
+let a = 300 
+
+
+if (true) {
+     let a = 10
+     const b = 20
+     var c = 30
+//  console.log("INNER:", a);
+}
+
+//  console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+function one (){
+    const username = "ayush"
+
+
+    function two() {
+      const website = "facebook"
+      console.log(username);
+    }
+     //     console.log(website);
+
+         two()
+}
+
+
+
+// one()
+
+
+if (true) {
+
+     const username = "sona"
+     if (username === "sona") {
+          const website = " instagram"
+          // console.log(username + website);
+          
+     }
+     // console.log(website);
+}
+
+// console.log(username);
+
+
+//++++++++++++++++++++++ interesting++++++++++++++
+console.log(addone(5));
+
+function addone(num) {
+     return num + 1
+     
+}
+
+
+ const addTwo = function num() {
+     return  num + 2
+     
+ }
+
+addTwo(5)
