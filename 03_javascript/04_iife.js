328c64cc590cd5a8e9c3ffb72ecb6f4707349832
Ayush Kumar Singh <147124011+AyushKumarSingh866@users.noreply.github.com>
2024-02-10 06:14:40 +0000

    IIFE

---
+++ 03_javascript/04_iife.js
@@ -0,0 +1,13 @@
+// Immediate Invoked Function Expressions (IIFE)
+
+
+(function february() {
+    console.log("DB CONNECTED");
+
+    
+})();
+
+( (name) => {
+console.log(`DB CONNECTED TWO ${name}`);
+
+})('Ayush')
