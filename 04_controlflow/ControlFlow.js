8ef75910eaf20014f45fec927867c2df0661e5c9
Ayush Kumar Singh <147124011+AyushKumarSingh866@users.noreply.github.com>
2024-02-13 17:10:06 +0000

    nullish operator

--- 04_controlflow/ControlFlow.js
+++ 04_controlflow/ControlFlow.js
@@ -63,3 +63,26 @@ if (userLoggedIn && debitcard ) {
 if (loggedInFromEmail || loggedInFromGoogle) {
     console.log("User logged in ");
 }
+
+// nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+
+
+
+console.log(val1);
+
+
+// terniary operator
+
+
+// condition ? true: false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
