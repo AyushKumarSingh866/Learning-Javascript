a68c5a64132452f5f23c1e7b4a7eab73b3243394
Ayush Kumar Singh <147124011+AyushKumarSingh866@users.noreply.github.com>
2024-02-13 15:00:40 +0000

    Switch case

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,20 @@
+const month = "march"
+
+switch (month) {
+    case "january":
+        console.log("This month is January");
+        break;
+    case "february":
+        console.log("This month is February");
+        break;
+    case "march":
+        console.log("This month is March");
+        break;
+    case "april":
+        console.log("This month is April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
